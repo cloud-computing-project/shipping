67998ecad7b35a55fe61c5f74430aacd5adb91bb
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-18 21:01:45 +0100

    upped version

--- models/src/main/resources/sql-scripts/init-shippings.sql
+++ models/src/main/resources/sql-scripts/init-shippings.sql
@@ -1,4 +1,4 @@
 INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (1, 1,'Slovenia','yes', 'fast');
-INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (2, 1,'Bosnia and Hercegovina','yes', 'regular');
-INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (1, 1,'Serbia','yes', 'regular');
+INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (2, 2,'Bosnia and Hercegovina','yes', 'regular');
+INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (3, 3,'Serbia','yes', 'regular');
 
