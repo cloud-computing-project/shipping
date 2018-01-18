4164c0157129b2665970f111e5620f40a8295c7c
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-18 20:59:36 +0100

    added some more data in sql script

--- models/src/main/resources/sql-scripts/init-shippings.sql
+++ models/src/main/resources/sql-scripts/init-shippings.sql
@@ -1,2 +1,4 @@
 INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (1, 1,'Slovenia','yes', 'fast');
+INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (2, 1,'Bosnia and Hercegovina','yes', 'regular');
+INSERT INTO shippings (id, product_id, shipping_to, service, delivery) VALUES (1, 1,'Serbia','yes', 'regular');
 
